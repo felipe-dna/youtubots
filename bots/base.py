69fb0f582d68714f69add99d573f8aca56a7a738
Felipe Amador <felipedavidamador@gmail.com>
2020-11-05 02:24:52 -0300

    feat: update the state handler to be a singleton

--- bots/base.py
+++ bots/base.py
@@ -1,7 +1,7 @@
 import logging
 
 from pprint import pformat
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 class Bot:
@@ -25,7 +25,7 @@ class Bot:
         self.logger.log(f'Starting...')
 
     async def stop(self) -> None:
-        self.logger.log(f'State updated: {pformat(self.state())}')
+        self.logger.log(f'State updated: {pformat(self.state.changed_fields)}')
         self.logger.log('Finishing...')
 
 
@@ -40,13 +40,36 @@ class Logger:
 
 
 class State:
-    def __init__(self, search_term: str = None, prefix: str = None) -> None:
+    __instance = None
+    changed_fields = ()
+
+    def __init__(
+            self,
+            search_term: str = None,
+            prefix: str = None,
+            source_content_original: str = None,
+            source_sanitized_content: str = None,
+            sentences: List[Dict[str, Any]] = []
+    ) -> None:
         self.search_term = search_term
         self.prefix = prefix
+        self.source_content_original = source_content_original
+        self.source_sanitized_content = source_sanitized_content
+        self.sentences = sentences
 
     def update(self, **kwargs):
+        __changed_fields = []
+
         for state_name, state_value in kwargs.items():
             self.__setattr__(state_name, state_value)
+            __changed_fields.append(state_name)
+
+        self.changed_fields = __changed_fields
 
     def __call__(self, *args, **kwargs):
         return self.__dict__
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
