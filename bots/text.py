8578185f7b3958e34249b5161e89adf021e24ecf
Felipe Amador <felipedavidamador@gmail.com>
2020-11-05 01:08:36 -0300

    feat: make the robot retrieve data from the wikipedia

--- bots/text.py
+++ bots/text.py
@@ -1,34 +1,43 @@
+import Algorithmia
+
+from decouple import config as env
+
 from bots.base import Bot
 
 
 class TextRobot(Bot):
     name = 'Text Robot'
 
-    async def ask_and_return_search_term(self) -> None:
-        search_term: str = input('\nType a search term: ')
-        self.state.update(search_term=search_term)
+    @staticmethod
+    async def instantiate_and_return_algorithmia_client() -> Algorithmia:
+        __algorithmia_api_key = env('ALGORITHMIA_API_KEY')
+        client = Algorithmia.client(__algorithmia_api_key)
+        return client
 
-    async def ask_and_return_prefix(self) -> None:
-        prefixes = ('who is', 'what is', 'The history of')
+    async def fetch_content_from_wikipedia(self) -> None:
+        client = await self.instantiate_and_return_algorithmia_client()
 
-        prefixes_with_indexes: str = '\n'.join(list(map(
-            lambda index_and_prefix_tuple: '[{0}] {1}'.format(
-                *index_and_prefix_tuple
-            ),
-            enumerate(iterable=prefixes, start=1)
-        )))
+        self.logger.log('Fetching content from Wikipedia...')
 
-        prefix_index: int = int(input(
-            f'\n{prefixes_with_indexes}\nChoose an option: '
-        ))
+        wikipedia_algorithm = client.algo('web/WikipediaParser/0.1.2')
 
-        self.state.update(prefix=prefixes[prefix_index - 1])
-        print('\n')
+        self.logger.log(f'Looking for {self.state.search_term} in Wikipedia...')
+        wikipedia_response = wikipedia_algorithm.pipe(self.state.search_term)
+        wikipedia_content = wikipedia_response.result.get('content')
+
+        print(wikipedia_content)
+
+    async def sanitize_content(self) -> None:
+        pass
+
+    async def break_content_into_sentences(self) -> None:
+        pass
 
     async def run(self) -> None:
         await super().run()
 
-        await self.ask_and_return_search_term()
-        await self.ask_and_return_prefix()
+        await self.fetch_content_from_wikipedia()
+        await self.sanitize_content()
+        await self.break_content_into_sentences()
 
         await self.stop()
