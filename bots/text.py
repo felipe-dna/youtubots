1bba6e8e7d590c5d7a79a3f862906d47c19ed31c
Felipe Amador <felipedavidamador@gmail.com>
2020-11-05 01:34:09 -0300

    feat: add method to remove the blank lines and markdown

--- bots/text.py
+++ bots/text.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import Algorithmia
 
 from decouple import config as env
@@ -14,6 +16,20 @@ class TextRobot(Bot):
         client = Algorithmia.client(__algorithmia_api_key)
         return client
 
+    async def remove_blank_lines_and_markdown(self, text: str) -> str:
+        self.logger.log('Removing blank lines and markdown from the text...')
+
+        all_lines: List[str] = text.split('\n')
+
+        text_without_blank_lines_and_markdown: List[str] = list(filter(
+            lambda line: (
+                False if len(line) == 0 or line.startswith('=') else True
+            ),
+            all_lines
+        ))
+
+        return ' '.join(text_without_blank_lines_and_markdown)
+
     async def fetch_content_from_wikipedia(self) -> None:
         client = await self.instantiate_and_return_algorithmia_client()
 
@@ -23,12 +39,18 @@ class TextRobot(Bot):
 
         self.logger.log(f'Looking for {self.state.search_term} in Wikipedia...')
         wikipedia_response = wikipedia_algorithm.pipe(self.state.search_term)
-        wikipedia_content = wikipedia_response.result.get('content')
+        wikipedia_content = wikipedia_response.result['content']
 
-        print(wikipedia_content)
+        self.state.update(source_content_original=wikipedia_content)
 
     async def sanitize_content(self) -> None:
-        pass
+        self.logger.log('Sanitizing wikipedia content...')
+
+        cleaned_text = await self.remove_blank_lines_and_markdown(
+            text=self.state.source_content_original
+        )
+
+        print(cleaned_text)
 
     async def break_content_into_sentences(self) -> None:
         pass
@@ -41,3 +63,4 @@ class TextRobot(Bot):
         await self.break_content_into_sentences()
 
         await self.stop()
+
